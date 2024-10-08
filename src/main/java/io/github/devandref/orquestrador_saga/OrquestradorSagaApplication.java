51b1c95e688ebeb4133e72bf8fe1c8f72214ac2d
devandref <yoandredepaula@gmail.com>
2024-10-07 21:44:10 -0300

    init

---
+++ src/main/java/io/github/devandref/orquestrador_saga/OrquestradorSagaApplication.java
@@ -0,0 +1,13 @@
+package io.github.devandref.orquestrador_saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrquestradorSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrquestradorSagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/devandref/orquestrador_saga/OrquestradorSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devandref.orquestrador_saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrquestradorSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
